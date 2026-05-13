13172df7e4243b442d96cc4eabac69cc65ff4531
tripathicle <tripathicle@gmail.com>
2026-05-13 22:33:07 +0530

    storage output

--- Module/StoragAccount/output.tf
+++ Module/StoragAccount/output.tf
@@ -1,5 +1,6 @@
 
 # Output variables for the storage account module
+#
 output "storage_account_name" {
   description = "The name of the storage account."
   value       = azurerm_storage_account.crocus-stg.name
